3442707a751e0d5c2d4f19403c7341bd97e890f3
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2024-04-26 18:28:00 -0300

    dia-1

---
+++ dia-1/teste.py
@@ -0,0 +1,34 @@
+from parsel import Selector
+import requests
+
+URL_BASE = "http://books.toscrape.com/catalogue/"
+
+# Vamos testar com a primeira página
+response = requests.get(URL_BASE + "page-1.html")
+selector = Selector(text=response.text)
+
+# Recupera o atributo href do primeiro elemento que combine com o seletor
+href = selector.css(".product_pod h3 a::attr(href)").get()
+detail_page_url = URL_BASE + href
+
+# Para obter a url completa, basta adicionar a nossa URL_BASE
+# print(URL_BASE + href)
+
+# baixa o conteúdo da página de detalhes
+detail_response = requests.get(detail_page_url)
+detail_selector = Selector(text=detail_response.text)
+
+# recupera a descrição do produto
+# ~ significa a tag irmã
+description = detail_selector.css("#product_description ~ p::text").get()
+print(description)
+
+# "Fatiamos" a descrição removendo o sufixo
+suffix = "...more"
+if description.endswith(suffix):
+    description = description[:-len(suffix)]
+print(description)
+
+# Extrai todos os preços da primeira página
+prices = selector.css(".product_price .price_color::text").re(r"£\d+\.\d{2}")
+print(prices)
